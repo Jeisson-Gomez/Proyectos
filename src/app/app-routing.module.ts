4e90e4d5e1a6eaa9c2c069fe7c716649e134fc19
Jeisson Gomez <71471533+jeypitbull@users.noreply.github.com>
2023-10-12 13:20:14 -0500

    Actualizacion del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AgregarEmpleadoComponent } from './componentes/agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './componentes/editar-empleado/editar-empleado.component';
+import { ListarEmpleadoComponent } from './componentes/listar-empleado/listar-empleado.component';
+
+const routes: Routes = [
+  { path: '', pathMatch:'full', redirectTo: 'AgregarEmpleadoComponent' },
+  { path: 'Agregar-Empleado', component: AgregarEmpleadoComponent },
+  { path: 'Editar-Empleado', component: EditarEmpleadoComponent },
+  { path: 'Editar-Empleado/:id', component: EditarEmpleadoComponent },
+  { path: 'Listar-Empleado', component: ListarEmpleadoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
